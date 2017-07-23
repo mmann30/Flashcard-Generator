8b4149c63f6bfc6c0a12e142e4c7af6cfc145b0e
mmann30 <michael.l.mann13@gmail.com>
2017-07-23 09:22:57 -0400

    initial function testing

--- basicCard.js
+++ basicCard.js
@@ -8,3 +8,10 @@
 //   * The constructed object should have a `front` property that contains the text on the front of the card.
 
 //   * The constructed object should have a `back` property that contains the text on the back of the card.
+
+function BasicCards(front, back) {
+	this.front = front;
+	this.back = back;
+}
+
+module.exports = BasicCards;
---
+++ main.js
@@ -0,0 +1,8 @@
+
+var BasicCard = require("./basicCard.js");
+
+var test = new BasicCard("front", "back");
+
+console.log(test.front);
+
+console.log(test.back);
