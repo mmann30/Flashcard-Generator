2ab43c45d76d985ea235a8b007c5fbda9e7764fa
mmann30 <michael.l.mann13@gmail.com>
2017-07-23 20:41:10 -0400

    main menu feature

--- main.js
+++ main.js
@@ -1,8 +1,28 @@
+// Flashcard Generator Main menu
 
-var BasicCard = require("./basicCard.js");
+var inquirer = require("inquirer");
+var create = require("./create.js");
 
-var test = new BasicCard("front", "back");
+// Displays main menu
+function mainMenu() {
+	inquirer.prompt([
+		{
+			type: "list",
+			name: "mainMenu",
+			message: "Main Menu: ",
+			choices: ["Play", "Create", "Exit"]
+		}
+	]).then(function(result) {
+		console.log(result.mainMenu);
+		if (result.mainMenu === "Play") {
+			console.log("Sorry, Play is currently offline");
+			mainMenu();
+		} else if (result.mainMenu === "Create") {
+			//run create()
+		} else {
+			return;
+		}
+	});
+}
 
-console.log(test.front);
-
-console.log(test.back);
+mainMenu();
