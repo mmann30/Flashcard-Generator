50c2f7bf7b3d5d79aaa92f5e9aad7018f4952b65
mmann30 <michael.l.mann13@gmail.com>
2017-07-24 13:00:35 -0400

    create clozeCard functional

--- clozeCard.js
+++ clozeCard.js
@@ -1,18 +1,10 @@
-
-function ClozeCard(text, cloze) {
+// Constructor function for a cloze card with a cloze statement
+// (statement with redacted text) and a full text side.
+ function ClozeCard(text, cloze) {
 	this.fullText = text;
 	this.cloze = cloze;
-	
-	// Regex method to remove the cloze from the fullText
-	this.partial = function(){
-		var regex = /this.cloze/i;
-		// Checks if the cloze is contained in the fullText
-		if (this.fullText.test(regex) === true) {
-			return this.fullText.replace(regex, "___");
-		} else {
-			console.log("Error: " + this.fullText + " does not contain " + this.cloze);
-		}
-	}
+	this.partial = text.replace(cloze, "___");
 }
 
 module.exports = ClozeCard;
+
