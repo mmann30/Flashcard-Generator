0dd84de9ea8695af5b716e331a19ef3c1f092a79
mmann30 <michael.l.mann13@gmail.com>
2017-07-23 20:40:35 -0400

    added prompts

--- create.js
+++ create.js
@@ -1,6 +1,36 @@
 // Create flashcards
 
-// Required files
+// Require files and packages
 var inquirer = require("inquirer");
 var BasicCard = require("./basicCard.js");
-var ClozeCard = require("./closeCard.js");
+var ClozeCard = require("./clozeCard.js");
+
+function createMain() {
+	inquirer.prompt([
+		{
+			type: "list",
+			name: "createMenu",
+			message: "What kind of flashcard would you like to make?",
+			choice: ["basic", "cloze"]
+		}
+	]).then(function(results) {
+		if (results.createMenu === "basic") {
+			createBasic();
+		} else {
+			createCloze();
+		}
+	});
+}
+
+function createBasic() {
+	//inquire user input for font,back
+	//create new instace of BasicCard
+}
+
+function createCloze() {
+	//inquire user input for text, cloze
+	//create new instace of ClozeCard
+}
+
+
+module.exports = createMain;
