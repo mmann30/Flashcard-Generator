3069d6b7d559e870f71f06a7295ee476ce6e7d2d
mmann30 <michael.l.mann13@gmail.com>
2017-07-23 20:39:21 -0400

    add regex functionallity

--- clozeCard.js
+++ clozeCard.js
@@ -1,20 +1,18 @@
-// * This file should define a Node module that exports a constructor for creating cloze-deletion flashcards, e.g.:
-//     `module.exports = ClozeCard;`
 
-//   * The constructor should accept two arguments: `text` and `cloze`.
-
-//   * The constructed object should have a `cloze` property that contains _only_ the cloze-deleted portion of the text.
-
-//   * The constructed object should have a `partial` property that contains _only_ the partial text.
-
-//   * The constructed object should have a `fullText` property that contains _only_ the full text.
-
-//   * The constructor should throw or log an error when the cloze deletion does _not_ appear in the input text.
-
-//   * Use prototypes to attach these methods, wherever possible.
-
-function ClozeCard() {
-
+function ClozeCard(text, cloze) {
+	this.fullText = text;
+	this.cloze = cloze;
+	
+	// Regex method to remove the cloze from the fullText
+	this.partial = function(){
+		var regex = /this.cloze/i;
+		// Checks if the cloze is contained in the fullText
+		if (this.fullText.test(regex) === true) {
+			return this.fullText.replace(regex, "___");
+		} else {
+			console.log("Error: " + this.fullText + " does not contain " + this.cloze);
+		}
+	}
 }
 
 module.exports = ClozeCard;
